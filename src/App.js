d27c52edd71b09c2edef43311f6aeb015090c1ec
OKELLO <okellomarvinkevin@gmail.com>
2023-03-03 06:02:44 +0300

    Concept of searching and filtering an array of values

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount(){
+    console.log('ComponentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then( (response) => 
     response.json()
@@ -20,8 +22,24 @@ class App extends Component {
   }
 
   render(){
+    console.log('render')
     return (
       <div className="App">
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='Seach Monsters'
+          onChange={(event)=>{
+            const searchString = event.target.value.toLocaleLowerCase();
+            console.log(searchString);
+            const filteredMonsters = this.state.monsters.filter((monster)=>{
+              return monster.name.toLocaleLowerCase().includes(event.target.value);
+            })
+            this.setState(()=>{
+              return {monsters: filteredMonsters}
+            })
+          }}
+        />
           {
             this.state.monsters.map((monster)=> {
               return <h1 key={monster.id}>{monster.name}</h1>
