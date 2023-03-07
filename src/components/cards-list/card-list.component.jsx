b1743b3731222720d0279f9bbe7c89c7766e2794
OKELLO <okellomarvinkevin@gmail.com>
2023-03-07 16:50:39 +0300

    Styling card list component

--- src/components/cards-list/card-list.component.jsx
+++ src/components/cards-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './card-list.style.css'
 
 export class CardList extends Component{
     render(){
@@ -6,10 +7,17 @@ export class CardList extends Component{
         console.log('render from cardList');
         const {monsters} = this.props;
         return (
-            <div>
-                {monsters.map(monster => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
+            <div className="card-list">
+                {monsters.map(monster => {
+                    const {name, email, id}= monster
+                    return(
+                    <div className='card-container' key={monster.id}>
+                        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                    )
+                    })}
             </div>
         )
     }
