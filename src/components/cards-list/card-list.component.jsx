5ce9bcf1dd850cb70ef9ce72eafe84955fe05900
OKELLO <okellomarvinkevin@gmail.com>
2023-03-07 17:19:44 +0300

    Created Card Component, added title

--- src/components/cards-list/card-list.component.jsx
+++ src/components/cards-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import './card-list.style.css'
+import Card from "../card/card.component";
 
 export class CardList extends Component{
     render(){
@@ -9,15 +10,10 @@ export class CardList extends Component{
         return (
             <div className="card-list">
                 {monsters.map(monster => {
-                    const {name, email, id}= monster
-                    return(
-                    <div className='card-container' key={monster.id}>
-                        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-                        <h2>{name}</h2>
-                        <p>{email}</p>
-                    </div>
-                    )
-                    })}
+                    return <Card monster={monster}/>
+                    }
+                 )
+                }
             </div>
         )
     }
