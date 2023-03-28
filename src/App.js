15a5392343868f9309564b3fade3776268ce5a2e
Marvin <marvin.okello@sophany.it>
2023-03-28 19:11:37 +0300

    using functional components, hooks ie useEffect, useState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,84 @@
-import {Component} from 'react';
-import {CardList} from './components/cards-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
+import {useEffect, useState} from "react";
+import CardList from "./components/cards-list/card-list.component";
 
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ''
+const App = () => {
+    console.log('render')
+    const [searchField, setSearchField] = useState('') // [value, setValue]
+    const [monsters, setMonsters] = useState([])
+    const [filteredMonsters, setFilteredMonsters] = useState(monsters)
+    console.log(searchField)
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) =>
+                response.json()
+            ).then((users) => setMonsters(users));
+    }, [])
+    useEffect(() => {
+        const newFilteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField);
+        })
+        setFilteredMonsters(newFilteredMonsters)
+    }, [monsters, searchField])
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
+        console.log(searchField)
     };
-  }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then( (response) => 
-    response.json()
-    ).then((users)=> this.setState(() => {
-      return {monsters: users}
-    }));
-  }
-
-  onSearchChange = (event)=>{
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(()=>{
-      return {searchField}
-    })
-  };
-
-  render(){
-    const {monsters, searchField} = this.state;
-    const {onSearchChange} = this;
-    const filteredMonsters = monsters.filter((monster)=>{
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    })
     return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeHolder={'Search Monsters'} className={'monsters-search-box'}/>
-        <CardList monsters= {filteredMonsters} />
-      </div>
+        <div className="App">
+            <h1 className='app-title'>Monsters Rolodex</h1>
+            <SearchBox onChangeHandler={onSearchChange} placeHolder={'Search Monsters'}
+                       className={'monsters-search-box'}/>
+            <CardList monsters={filteredMonsters}/>
+        </div>
     );
-  }  
+
 }
+
+/*class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) =>
+                response.json()
+            ).then((users) => this.setState(() => {
+            return {monsters: users}
+        }));
+    }
+
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase();
+        this.setState(() => {
+            return {searchField}
+        })
+    };
+
+    render() {
+        const {monsters, searchField} = this.state;
+        const {onSearchChange} = this;
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLocaleLowerCase().includes(searchField);
+        })
+        return (
+            <div className="App">
+                <h1 className='app-title'>Monsters Rolodex</h1>
+                <SearchBox onChangeHandler={onSearchChange} placeHolder={'Search Monsters'}
+                           className={'monsters-search-box'}/>
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
+}*/
 
 export default App;
